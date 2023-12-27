0d897445c2a8f710c70fac95db04ccf464b047bb
Felipe Morilho <felipemdecastro@gmail.com>
2023-12-27 11:34:45 -0300

    Projeto do Lab Bootcamp Android DIO

---
+++ app/src/main/java/com/example/appbootcampdio/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.appbootcampdio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.appbootcampdio.ui.theme.AppbootcampDIOTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val message = getString(R.string.title)
+        setContent {
+            AppbootcampDIOTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(message)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "$name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppbootcampDIOTheme {
+        Greeting("Android")
+    }
+}
